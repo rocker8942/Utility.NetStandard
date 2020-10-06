521c3294007bc2591e206159a5d986dd1926c6ed
joe park <rocker8942@gmail.com>
2020-10-06 21:24:27 +0800

    Fix bug on CSVHelper.

--- Utility.Test/CSV/CSVHelperTest.cs
+++ Utility.Test/CSV/CSVHelperTest.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Text;
+using FluentAssertions;
 using Moq;
 using Xunit;
 
@@ -17,7 +20,7 @@ namespace Utility.Test.CSV
             // arrange
             var mockIOHelper = new Mock<IIOHelper>();
 
-            var stream = new MemoryStream(Encoding.UTF8.GetBytes("id,email\r\n1,id1,test1.com\r\nid2,test2.com"));
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes("id,email\r\nid1,test1.com\r\nid2,test2.com"));
             using (var streamReader = new StreamReader(stream))
             {
                 // act
@@ -27,6 +30,12 @@ namespace Utility.Test.CSV
                 // assert
                 var expected = new Collection<string> {"id", "email"};
                 Assert.Equal(expected, csv.ColumnList);
+
+                var recordExpected = new List<string>()
+                {
+                    "id1","test1.com"
+                };
+                recordExpected.Should().BeEquivalentTo(csv.Rows[0].ItemArray.Select(a => a.ToString()).ToList());
             }
         }
     }
