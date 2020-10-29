cc0f5623c0e10059e2f400ffc38f49aa363f7bff
joe park <rocker8942@gmail.com>
2020-10-29 22:53:02 +0800

    Add UuDecode function

---
+++ Utility.Test/Encryption/UuDecodeTest.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using Utility.Encryption;
+using Xunit;
+
+namespace Utility.Test.Encryption
+{
+    public class UuDecodeTest
+    {
+        [Fact]
+        public void UuDecodeTestRun()
+        {
+            var text = File.ReadAllText("UuEncodedSample.txt");
+            UuEncodingHelper.UuDecode(text, "test.jpg");
+        }
+    }
+}
